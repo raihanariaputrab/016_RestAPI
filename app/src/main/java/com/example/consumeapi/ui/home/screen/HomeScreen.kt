1d1fcaf332ba951410cd1651fe1b4367790ea4ae
raihanariaputrab <arpb.business@gmail.com>
2023-12-27 00:29:38 +0700

    add spacer and icon button

--- app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScopeInstance.weight
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,10 +16,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -110,6 +113,13 @@ fun KontakCard(
                     text = kontak.email,
                     style = MaterialTheme.typography.titleMedium,
                 )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = {onDeleteClick}) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription =  null,
+                    )
+                }
             }
         }
     }

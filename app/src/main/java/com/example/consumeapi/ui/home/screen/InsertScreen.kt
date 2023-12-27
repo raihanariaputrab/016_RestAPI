e55c9cc7d78e68eb3f132e45361c9e9e8055ccec
raihanariaputrab <arpb.business@gmail.com>
2023-12-27 22:07:29 +0700

    add column in insert screen

--- app/src/main/java/com/example/consumeapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/home/screen/InsertScreen.kt
@@ -1,7 +1,10 @@
 package com.example.consumeapi.ui.home.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.consumeapi.ui.home.viewmodel.InsertUiEvent
 
 @Composable
@@ -11,5 +14,10 @@ fun FormInputSiswa(
     onValueChange: (InsertUiEvent) -> Unit = {},
     enabled: Boolean = true
 ) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
 
+    }
 }

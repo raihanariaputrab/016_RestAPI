3365ce29ac1aa7e779258b2b3443ca2ba8631044
raihanariaputrab <arpb.business@gmail.com>
2023-12-27 19:22:10 +0700

    add insert view model

--- app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
@@ -1,13 +1,11 @@
 package com.example.consumeapi.ui.home.screen
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScopeInstance.weight
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,12 +14,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -36,8 +33,10 @@ import com.example.consumeapi.ui.home.viewmodel.KontakUIState
 
 
 @Composable
-fun HomeScreen(
-    kontakUIState: KontakUIState, retryAction: () -> Unit, modifier: Modifier = Modifier
+fun HomeStatus(
+    kontakUIState:KontakUIState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
 ){
     when (kontakUIState){
         is KontakUIState.Loading -> OnLoading (modifier = modifier.fillMaxSize())
@@ -47,91 +46,79 @@ fun HomeScreen(
         is KontakUIState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
     }
 }
+
 @Composable
-fun OnLoading(modifier: Modifier = Modifier){
-    Image(
-        modifier = modifier.size(200.dp),
-        painter = painterResource(R.drawable.loading_img),
-        contentDescription = stringResource(R.string.loading)
-    )
-}
-@Composable
-fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier){
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Image(painter = painterResource(id = R.drawable.ic_connection_error), contentDescription ="")
-    }
-}
-@Composable
-fun KontakLayout(
-    kontak: List<Kontak>,
-    modifier: Modifier = Modifier,
-    onDetailClick: (Kontak) -> Unit,
-    onDeleteClick: (Kontak) -> Unit = {}
-){
+fun KontakLayout(kontak: List<Kontak>,  modifier: Modifier = Modifier) {
     LazyColumn(
         modifier = modifier,
         contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ){
         items(kontak) {kontak ->
-            KontakCard(
-                kontak = kontak,
-                modifier = Modifier
+            KontakCard(kontak =  kontak, modifier = Modifier
                 .fillMaxWidth()
-                .clickable {onDetailClick(kontak)},
-            onDeleteClick = {
-                onDeleteClick(kontak)
-            }
-            )
+                .clickable { })
         }
     }
+
 }
+
 @Composable
-fun KontakCard(
-    kontak: Kontak,
-    onDeleteClick: (Kontak) -> Unit = {},
-    modifier: Modifier = Modifier
-){
+fun KontakCard(kontak: Kontak, modifier: Modifier = Modifier) {
     Card(
         modifier = modifier,
         shape = MaterialTheme.shapes.medium,
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
-    )  {
-        Column(
+    ){
+        Column (
             modifier = Modifier.padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ){
-            Row(
+            Row (
                 modifier = Modifier.fillMaxWidth(),
             ){
                 Text(
                     text = kontak.nama,
                     style = MaterialTheme.typography.titleLarge,
                 )
-                Spacer(Modifier.weight(1f))
-                Icon(
-                    imageVector = Icons.Default.Phone,
+                Spacer(modifier.weight(1f))
+                Icon(imageVector = Icons.Default.Phone,
                     contentDescription = null,
                 )
                 Text(
                     text = kontak.telpon,
                     style = MaterialTheme.typography.titleMedium
                 )
-                Text(
-                    text = kontak.email,
-                    style = MaterialTheme.typography.titleMedium,
-                )
-                Spacer(Modifier.weight(1f))
-                IconButton(onClick = {onDeleteClick}) {
-                    Icon(
-                        imageVector = Icons.Default.Delete,
-                        contentDescription =  null,
-                    )
-                }
             }
+            Text(
+                text = kontak.telpon,
+                style = MaterialTheme.typography.titleMedium)
+        }
+    }
+
+
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(id = R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = "")
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
         }
     }
 }
